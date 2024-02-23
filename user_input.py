ba06c0733f3a0e5ecf04f32119a2ad73cf9600c9
Omofoye Mary <120613660+MaryTee1212@users.noreply.github.com>
2024-02-22 22:10:02 -0800

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter username:");
+age = input("How old are you?:");
+location = input("Where do you live?: ");
+print("Hello {}, you are {}, you live in {}.".format(name, age, location));
